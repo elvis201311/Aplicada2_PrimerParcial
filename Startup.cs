27a562db1e3b1ce73a76e958493bfe1a4986bf2c
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-10-06 17:30:32 -0400

    Listo Para el Parcial

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Aplicada2_PrimerParcial.Data;
+using Blazored.Toast;
 
 namespace Aplicada2_PrimerParcial
 {
@@ -26,6 +27,7 @@ namespace Aplicada2_PrimerParcial
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddBlazoredToast();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
