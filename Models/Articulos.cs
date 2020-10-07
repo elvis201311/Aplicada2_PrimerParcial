c110aa28d9c789ea9949f13258b3b0d1c21e1fd9
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-10-07 18:25:25 -0400

    Articulos Lista

--- Models/Articulos.cs
+++ Models/Articulos.cs
@@ -1,5 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+
+namespace PrimerParcial.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public int Costo { get; set; }
+        public int ValorInventario { get; set; }
+
+        public Articulos()
+        {
+            ArticuloId = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Costo = 0;
+            ValorInventario = 0;
+        }
+    }
+}
